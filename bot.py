a904b7c61bac73ae386174cc85eb693bf7f234ad
allelala56 <maketrade2021@gmail.com>
2025-04-23 12:25:56 +0200

    Delete bot.py

--- bot.py
+++
@@ -1,5 +0,0 @@
-# Ligne corrigée (exemple) :
-label = f'{s["name"]} - {s["price"]}€'
-# Devient :
-label = f'{s["name"]} - {s["price"]}€'
-markup.add(telebot.types.InlineKeyboardButton(label, callback_data=f"BUY_{s['id']}"))
